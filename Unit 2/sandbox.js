4b7a741d79d215131f0dff77903789715319cd93
Ineedhelp25 <rkim07626@gmail.com>
2023-10-18 20:43:38 -0400

    .

---
+++ Unit 2/sandbox.js
@@ -0,0 +1,42 @@
+let age = 25;
+let year = 2023;
+
+console.log(age, year)
+
+let firstName = "Elle";
+let lastName = "Kim";
+
+let fullName = firstName = firstName + " " + lastName;
+console.log(fullName);
+
+console.log(fullName.length);
+
+console.log(fullName[2]);
+
+console.log(fullName.toUpperCase());
+
+
+let date = 17;
+if (date === "17") {
+    console.log("yay!");
+}
+
+if (date === "17") {
+    console.log("yeah!");
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+console.log(add(1, 2));
+
+function printHi (name) {
+    return "hi " + name;
+}
+
+function printBye(func) {
+    return func("Jolene ") + "bye";
+}
+
+console.log(printBye(printHi));
